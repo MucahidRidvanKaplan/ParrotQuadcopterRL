0a59508ce7e9d96ede20ab3854f8b22faea30d59
mehmetsakarya <m.sakarya_1998@hotmail.com>
2024-05-05 18:34:30 +0300

    Drone environment created (should be checked)

---
+++ DroneEnvironment.m
@@ -0,0 +1,81 @@
+classdef DroneEnvironment < rl.env.MATLABEnvironment
+    properties
+        % Drone dynamics parameters
+        MaxVelocity = 1; % Maximum velocity of the drone
+        TimeStep = 0.1; % Time step for simulation
+        
+        % Environment parameters
+        SquareSize = 5; % Size of the square (meters)
+        StartPosition = [0, 0]; % Starting position of the drone
+        CurrentPosition; % Current position of the drone
+        StepCount = 0; % Step count for episode termination
+    end
+    
+    methods
+        function this = DroneEnvironment()
+            % Constructor
+            this.CurrentPosition = this.StartPosition;
+        end
+        
+        function [observation, reward, isDone] = step(this, action)
+            % Apply action to the environment and simulate one step
+            
+            % Convert action to velocity commands
+            velocity = this.MaxVelocity * action;
+            
+            % Update drone position based on velocity commands
+            this.CurrentPosition = this.CurrentPosition + velocity * this.TimeStep;
+            
+            % Calculate reward
+            reward = this.calculateReward();
+            
+            % Check if episode is done based on termination conditions
+            isDone = this.isEpisodeDone();
+            
+            % Increment step count
+            this.StepCount = this.StepCount + 1;
+            
+            % Return observation
+            observation = this.getObservation();
+        end
+        
+        function initialObservation = reset(this)
+            % Reset the environment to an initial state
+            
+            % Reset drone position
+            this.CurrentPosition = this.StartPosition;
+            
+            % Reset step count
+            this.StepCount = 0;
+            
+            % Return initial observation
+            initialObservation = this.getObservation();
+        end
+        
+        function observation = getObservation(this)
+            % Get observation (state) from the environment
+            
+            % For simplicity, observation is the current position of the drone
+            observation = this.CurrentPosition;
+        end
+        
+        function reward = calculateReward(this)
+            % Calculate reward based on current state
+            
+            % For simplicity, reward is based on distance to the target (center of the square)
+            targetPosition = this.StartPosition;
+            distanceToTarget = norm(this.CurrentPosition - targetPosition);
+            reward = -distanceToTarget; % Negative distance as reward (closer to target is better)
+        end
+        
+        function isDone = isEpisodeDone(this)
+            % Check if episode is done (termination conditions)
+            
+            % Terminate episode after drawing the square and returning to the start position
+            isDone = false;
+            if this.StepCount >= (4 * this.SquareSize / this.MaxVelocity) + 1 % Plus 1 for returning to start position
+                isDone = true;
+            end
+        end
+    end
+end
